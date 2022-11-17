120174a37ba442aa87baa3e0fc0372acdb01972f
huyf <724826175@qq.com>
2022-11-17 16:08:53 +0800

    添加死信队列测试

--- src/main/java/com/tony/rabbitmq_producer/config/TopicRabbitConfig.java
+++ src/main/java/com/tony/rabbitmq_producer/config/TopicRabbitConfig.java
@@ -1,9 +1,6 @@
 package com.tony.rabbitmq_producer.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +8,9 @@ import org.springframework.context.annotation.Configuration;
 public class TopicRabbitConfig {
     public static final String man = "topic.man";
     public static final String woman = "topic.woman";
+    public static final String ttl = "topic.ttl";
+    public static final String test_dlx = "test_dlx.message";
+    public static final String dlx = "dlx.message";
 
     @Bean
     public Queue firstQueue() {
@@ -23,9 +23,42 @@ public class TopicRabbitConfig {
     }
 
     @Bean
+    public Queue thirdQueue() {
+        //队列消息统一过期100s
+        return QueueBuilder.durable(ttl).ttl(100000).build();
+    }
+
+    @Bean
+    Queue fourthQueue() {
+        //测试死信队列的队列，绑定死信交换机，死信队列路由键
+        return QueueBuilder.durable(test_dlx).ttl(10000).deadLetterExchange("dlxExchange")
+                .deadLetterRoutingKey("dlx.test").maxLength(10).build();
+    }
+
+    @Bean
+    Queue fifthQueue() {
+        //创建死信队列
+        return QueueBuilder.durable(dlx).build();
+    }
+
+    @Bean
     public TopicExchange topicExchange() {
         return new TopicExchange("topicExchange", true, false);
     }
+
+    @Bean
+    public TopicExchange testDlxExchange() {
+        //创建死信交换机
+        return new TopicExchange("testDlxExchange", true, false);
+    }
+
+
+    @Bean
+    public TopicExchange dlxExchange() {
+        //创建死信交换机
+        return new TopicExchange("dlxExchange", true, false);
+    }
+
 
     @Bean
     public Binding bindingWithTopicMan() {
@@ -36,4 +69,22 @@ public class TopicRabbitConfig {
     public Binding bindingWithTopicAll() {
         return BindingBuilder.bind(secondQueue()).to(topicExchange()).with("topic.#");
     }
+
+    @Bean
+    public Binding bindingWithTopicTTL() {
+        return BindingBuilder.bind(thirdQueue()).to(topicExchange()).with("topic.ttl");
+    }
+
+    @Bean
+    public Binding bindingWithTestDlx(){
+        return BindingBuilder.bind(fourthQueue()).to(testDlxExchange()).with("test.#");
+    }
+
+
+    @Bean
+    public Binding bindingWithDlx() {
+        //绑定死信队列到死信交换机
+        return BindingBuilder.bind(fifthQueue()).to(dlxExchange()).with("dlx.#");
+    }
+
 }
--- src/main/java/com/tony/rabbitmq_producer/controller/SendMessageController.java
+++ src/main/java/com/tony/rabbitmq_producer/controller/SendMessageController.java
@@ -2,6 +2,9 @@ package com.tony.rabbitmq_producer.controller;
 
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.lang.UUID;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -57,7 +60,10 @@ public class SendMessageController {
         map.put("messageData", messageData);
         map.put("createTime", createTime);
 
-        rabbitTemplate.convertAndSend("topicExchange", "topic.man", map);
+        for (int i = 0; i < 10; i++) {
+            rabbitTemplate.convertAndSend("topicExchange", "topic.man", map);
+        }
+
         return "ok";
     }
 
@@ -72,6 +78,67 @@ public class SendMessageController {
         map.put("createTime", createTime);
 
         rabbitTemplate.convertAndSend("topicExchange", "topic.woman", map);
+        return "ok";
+    }
+
+    @GetMapping("/topic/3")
+    public String sendTopic3() {
+        String messageId = UUID.randomUUID().toString();
+        String messageData = "hello topic message : topic.ttl";
+        String createTime = DateUtil.now();
+        Map<String, String> map = new HashMap<>();
+        map.put("messageId", messageId);
+        map.put("messageData", messageData);
+        map.put("createTime", createTime);
+
+        for (int i = 0; i < 5; i++) {
+            rabbitTemplate.convertAndSend("topicExchange", "topic.ttl", map);
+        }
+
+        return "ok";
+    }
+
+    @GetMapping("/topic/4")
+    public String sendTopic4() {
+        String messageId = UUID.randomUUID().toString();
+        String messageData = "hello topic message : topic.ttl";
+        String createTime = DateUtil.now();
+        Map<String, String> map = new HashMap<>();
+        map.put("messageId", messageId);
+        map.put("messageData", messageData);
+        map.put("createTime", createTime);
+
+
+        //设置消息单独过期 5秒过期
+        MessagePostProcessor processor = new MessagePostProcessor() {
+            @Override
+            public Message postProcessMessage(Message message) throws AmqpException {
+                message.getMessageProperties().setExpiration("5000");
+                return message;
+            }
+        };
+
+        rabbitTemplate.convertAndSend("topicExchange", "topic.ttl", map, processor);
+
+        return "ok";
+    }
+
+
+    @GetMapping("/dlx")
+    public String sendTopicDlx() {
+        String messageId = UUID.randomUUID().toString();
+        String messageData = "hello topic message : test.message";
+        String createTime = DateUtil.now();
+        Map<String, String> map = new HashMap<>();
+        map.put("messageId", messageId);
+        map.put("messageData", messageData);
+        map.put("createTime", createTime);
+
+//        for (int i = 0; i < 20; i++) {
+//            rabbitTemplate.convertAndSend("testDlxExchange", "test.message", map);
+//        }
+
+        rabbitTemplate.convertAndSend("testDlxExchange", "test.message", map);
         return "ok";
     }
 }
